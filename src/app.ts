c4d2fef33e7b2eb83b6a65dd3c9bb6823ae5368c
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-11-24 19:39:13 -0300

    Prisma and DB Configuration

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,14 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
+export const app = fastify();
 
-export const app = fastify()
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "",
+    email: "",
+  },
+});
+
