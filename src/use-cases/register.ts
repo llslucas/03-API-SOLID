54fd55bfe252699b5793e8e1e28ed624698a866c
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-12-07 18:35:40 -0300

    Caso de uso de criação de usuário implementado

---
+++ src/use-cases/register.ts
@@ -0,0 +1,32 @@
+import { hash } from "bcryptjs";
+import { UserRepository } from "@/repositories/users-repository";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UserRepository) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6);
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError();
+    }
+
+    // const prismaUsersRepository = new PrismaUsersRepository();
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+  }
+}
+
