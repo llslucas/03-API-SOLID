3d27c67cb438e01ad408e1fa025ebd2aa7c7e25a
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-12-08 10:41:54 -0300

    Configuração do vitest e primeira suite de testes

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcryptjs";
-import { UserRepository } from "@/repositories/users-repository";
+import { UsersRepository } from "@/repositories/users-repository";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+import { User } from "@prisma/client";
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -8,10 +9,18 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
-export class RegisterUseCase {
-  constructor(private usersRepository: UserRepository) {}
+interface RegisterUseCaseResponse {
+  user: User;
+}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6);
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -22,11 +31,15 @@ export class RegisterUseCase {
 
     // const prismaUsersRepository = new PrismaUsersRepository();
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+
+    return {
+      user,
+    };
   }
 }
 
