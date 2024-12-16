afb4f5f8010df03ed6b4f59c44088be29d4236d9
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-12-16 08:59:10 -0300

    Completed: Deve ser possível o usuário obter o seu histórico de check-ins

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,6 +2,7 @@ import { CheckIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
+  findManyByUserId(userId: string): Promise<CheckIn[]>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
 }
 
