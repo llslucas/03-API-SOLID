d3368607b8a4cc33427dbb6bab049d747f9d277e
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-12-16 17:45:11 -0300

    Completed: Deve ser possível validar o check-in de um usuário

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,40 @@ import dayjs from "dayjs";
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn: CheckIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      created_at: new Date(),
+    };
+
+    this.items.push(checkIn);
+
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => (item.id = checkIn.id));
+
+    if (checkInIndex >= 0) {
+      this.items[0] = checkIn;
+    }
+
+    return checkIn;
+  }
+
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
+
+    if (!checkIn) {
+      return null;
+    }
+
+    return checkIn;
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf("date");
     const endOfTheDay = dayjs(date).endOf("date");
@@ -29,20 +63,6 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return this.items
       .filter((item) => item.user_id === userId)
       .slice((page - 1) * 20, page * 20);
-  }
-
-  async create(data: Prisma.CheckInUncheckedCreateInput) {
-    const checkIn: CheckIn = {
-      id: randomUUID(),
-      user_id: data.user_id,
-      gym_id: data.gym_id,
-      validated_at: data.validated_at ? new Date(data.validated_at) : null,
-      created_at: new Date(),
-    };
-
-    this.items.push(checkIn);
-
-    return checkIn;
   }
 
   async countByUserId(userId: string): Promise<number> {
